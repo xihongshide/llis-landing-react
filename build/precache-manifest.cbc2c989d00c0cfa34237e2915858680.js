cb8637cf2b21f73bff4379fe72860648d528e234
LeonLiangLi <liangli.0311@gmail.com>
2019-09-02 16:28:30 -0400

    init react app

---
+++ build/precache-manifest.cbc2c989d00c0cfa34237e2915858680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4bed627fe8af414d29f76fbe8fd788",
+    "url": "/xihongshide/llis-landing-react/index.html"
+  },
+  {
+    "revision": "68a4733653fc8ab0fd5b",
+    "url": "/xihongshide/llis-landing-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ea2724fe7fc79f36df09",
+    "url": "/xihongshide/llis-landing-react/static/js/2.30933d28.chunk.js"
+  },
+  {
+    "revision": "68a4733653fc8ab0fd5b",
+    "url": "/xihongshide/llis-landing-react/static/js/main.b3d3831a.chunk.js"
+  },
+  {
+    "revision": "8042a491926d98b42bfa",
+    "url": "/xihongshide/llis-landing-react/static/js/runtime~main.cdeb1ed5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xihongshide/llis-landing-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
