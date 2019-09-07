15898f2c2c32407455d40960a3e63d24b05a811d
LeonLiangLi <liangli.0311@gmail.com>
2019-09-06 20:47:02 -0400

    Updates

---
+++ precache-manifest.4b1c61d3003581b213c2f42301db6f50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c85fb5e678d1ac2b035079a88e5b5b",
+    "url": "/xihongshide/llis-landing-react/index.html"
+  },
+  {
+    "revision": "b7bea5e835d254b792ec",
+    "url": "/xihongshide/llis-landing-react/static/css/main.5e23ed28.chunk.css"
+  },
+  {
+    "revision": "cafa34600d5c3ce7ae81",
+    "url": "/xihongshide/llis-landing-react/static/js/2.24b8a15f.chunk.js"
+  },
+  {
+    "revision": "b7bea5e835d254b792ec",
+    "url": "/xihongshide/llis-landing-react/static/js/main.ae9e2ed4.chunk.js"
+  },
+  {
+    "revision": "8042a491926d98b42bfa",
+    "url": "/xihongshide/llis-landing-react/static/js/runtime~main.cdeb1ed5.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/xihongshide/llis-landing-react/static/media/logo.d41d8cd9.svg"
+  }
+]);
