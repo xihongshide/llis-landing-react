1700796c5c08974bad842ef01f6ba70501f1cd94
LeonLiangLi <liangli.0311@gmail.com>
2019-12-05 21:54:55 -0500

    start working on blog

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import CodeAniElements from './partialComponents/codeView.js';
 
 class Blog extends Component {
    render() {
       return (
          <div className="content blog">
-            <div className="container">
+            <div className="container blog-container">
                <div className="inner-container">
-                  <h1>under construction...</h1>
+                   <h1>&lt; Building &frasl; &gt;</h1>
+                   <CodeAniElements />
                </div>
             </div>
          </div>
