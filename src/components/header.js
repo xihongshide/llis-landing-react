8dd3fc138d327b513106ad48a61f07c99a6e3735
Liang Li <liangli.0311@gmail.com>
2019-12-11 16:18:32 -0500

    mobile nav

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,34 @@ import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.closeNavbar = this.closeNavbar.bind(this);
+        this.state = {
+            isOpen: false,
+        };
+    }
+
+    toggle() {
+        console.log("toggle");
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    closeNavbar() {
+        console.log("close");
+        this.setState({
+            isOpen: false
+        });
+    }
+
     render() {
         return (
             <div className="header">
-                <Navbar bg="light" expand="lg">
+                <Navbar bg="light" expand="lg" expanded={this.state.isOpen}>
                     <Navbar.Brand className="nav-logo">
                         <NavLink to="/">
                             <span>L</span>
@@ -16,7 +40,7 @@ class Header extends Component {
                         </NavLink>
                     </Navbar.Brand>
 
-                    <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={this.toggle}>
                         <span></span>
                         <span></span>
                         <span></span>
@@ -26,19 +50,19 @@ class Header extends Component {
                         <Nav className="mr-auto nav-list">
                             <div>
                                 <ul>
-                                    <li>
+                                    <li onClick={this.closeNavbar}>
                                         <NavLink exact to="/" activeClassName="selected" className="home">Home</NavLink>
                                     </li>
-                                    <li>
+                                    <li onClick={this.closeNavbar}>
                                         <span>/</span><NavLink to="/about" activeClassName="selected" className="about">About</NavLink>
                                     </li>
-                                    <li>
+                                    <li onClick={this.closeNavbar}>
                                         <span>/</span><NavLink to="/skills" activeClassName="selected" className="skills">Skills</NavLink>
                                     </li>
-                                    <li>
+                                    <li onClick={this.closeNavbar}>
                                         <span>/</span><NavLink to="/contact" activeClassName="selected" className="contact">Contact</NavLink>
                                     </li>
-                                    <li>
+                                    <li onClick={this.closeNavbar}>
                                         <span>/</span><NavLink to="/blog" activeClassName="selected" className="blog">Blog</NavLink>
                                     </li>
                                 </ul>
