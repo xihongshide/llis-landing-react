a4ba0be6c2c33572a18bcce37d71565dd6af5889
lli <lli@launchfire.com>
2023-09-19 17:47:55 -0700

    remove firebase deploy files and code lines

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {fire, analytics} from './firebase';
 
 require('velocity-animate');
 require('velocity-animate/velocity.ui');
